09fc19552de1d2e36f637613cca2f0d96c51373f
jhon <jhon.bocanegra26@gmail.com>
2023-06-08 16:12:06 -0500

    recibir y validar los datos

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,11 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsString()
+  @MinLength(2)
+  name: string;
+
+  @IsInt()
+  @IsPositive()
+  no: number;
+}
